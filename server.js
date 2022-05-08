042a28a20e2213fd8883874ce91a3abcaf7980a5
arvindsuriyaa <arvinds2605@gmail.com>
2022-05-08 18:32:16 +0530

    client UI

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const app = express();
 
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT);
-
-app.get("/", (req, res) => {
-  res.send({ message: "App is up and running" });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
